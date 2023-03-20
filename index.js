6d52894a3c85c857480c93fc414356b2edbfdfd0
Martin Klingenberg <martin.klingenberg@alv.no>
2023-03-20 09:04:48 +0100

    Simple endpoints

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * - Top worst resturants
+ * - Simple fuzzy "can I eat here"
+ * Question how would you solve this for a
+ */
+console.log("yay");
--- index.mjs
+++ index.mjs
@@ -1,6 +1,12 @@
 import express from "express";
 import { data } from "./data.mjs";
 
+
+const TOTAL_SCORE_KEY="total_karakter";
+const CHOSEN_AREA="OSLO";
+const POSTAL_CODE_KEY="poststed";
+const osloData = data.entries.filter(entry => entry[POSTAL_CODE_KEY] === CHOSEN_AREA);
+
 const APP_PORT = 8080;
 /**
  * - Top worst resturants
@@ -16,10 +22,20 @@ app.get("/test", (req, res) => {
   res.send({ a: "Express er moderat lett" });
 });
 
-app.get("/best", (req, res) => {});
+app.get("/best", (req, res) => {
+  res.send(osloData.filter(entry => readKey(entry[TOTAL_SCORE_KEY]) === 0));
+});
 
-app.get("/worst", (req, res) => {});
+app.get("/worst", (req, res) => {
+  res.send(osloData.filter(entry => readKey(entry[TOTAL_SCORE_KEY]) > 2));
+});
+
 
 app.listen(APP_PORT, () =>
   console.log(`Matilsynet lytter på port ${APP_PORT}`)
 );
+
+
+function readKey(value) {
+  return parseInt(value, 10);
+}
